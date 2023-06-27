d3075f6f90bf6402901df7ae99e5d6ab7d165938
it222199 <it222199@GAME222199>
2023-06-27 19:12:02 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <Windows.h>
+#include <iostream>
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+	//	引数は使わないよ☆
+	UNREFERENCED_PARAMETER(hInstance);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(nCmdShow);
+
+#ifdef _DEBUG
+	// メモリリークチェックのためのデバッグフラグを設定するよ☆
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	// レポートモードを設定するよ☆
+	_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_DEBUG);
+#endif // _DEBUG
+
+	//Crown::Engine* crown = Crown::Engine::GetEngine();
+
+#ifdef _DEBUG
+	//crown->UseLogConsole();
+	//crown->UseLogFile();
+#endif // _DEBUG
+	//crown->Execution();
+	//crown->DeleteEngine();
+	return 0;
+}
